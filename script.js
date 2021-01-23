e1eb899d66fa8611b83233adfb617fa5809005c1
Dave Marong <kmarong@hotmail.com>
2021-01-23 14:41:01 +0100

    Completed "create new function", for 1 more
    - Now you can add one new calendar, but only one more.

--- script.js
+++ script.js
@@ -9,11 +9,16 @@ function myFunction() {
 }
 
 /*----------------------MY CHALLENGE----------*/
+let calendar = document.querySelector("#calendar");
+let createNewBtn = document.querySelector("#createCalendar");
+let clone = calendar.cloneNode(true);
+
 /*-------Style "completed" and "uncompleted" mark on days----------*/
-let days = document.getElementsByClassName("day");
+let days = document.getElementsByClassName("day0");
 let daysArray = Array.from(days);
 
 daysArray.forEach(function(current) {
+    current.classList.remove("day0");
     current.addEventListener("click", function() {
         if (current.classList.contains("purpleHover")){
             current.classList.remove("purpleHover")
@@ -27,17 +32,32 @@ daysArray.forEach(function(current) {
 })
 
 
-
 /*-------Create new calendar----------*/
-let calendar = document.querySelector("#calendar");
-let createNewBtn = document.querySelector("#createCalendar");
-let clone = calendar.cloneNode(true);
+
 
 function newId(){ 
+    
     let newNumber = 2;
     clone.id = "calendar" + newNumber;
     newNumber += 1;
     calendar.after(clone);
+    
+    let days2 = document.getElementsByClassName("day0");
+    let daysArray2 = Array.from(days2);
+    createNewBtn.addEventListener("click", newId);
+    daysArray2.forEach(function(current) {
+    current.classList.remove("day0");
+    current.addEventListener("click", function() {
+        if (current.classList.contains("purpleHover")){
+            current.classList.remove("purpleHover")
+            current.classList.add("correct")
+        }
+        else{
+            current.classList.add("purpleHover")
+            current.classList.remove("correct")
+        }
+    });
+})
     
 }
 createNewBtn.addEventListener("click", newId);
