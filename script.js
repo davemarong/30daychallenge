02398c82ec55df2061a8bbcda798882fc40d276e
Dave Marong <kmarong@hotmail.com>
2021-01-23 10:25:29 +0100

    Day style changes to green when clicked
    - Each day on the calendar changes to green when you click them, but they dont go back when you click again.

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 function myFunction() {
-  var x = document.getElementById("nav");
-  if (x.className === "nav") {
-    x.className += " responsive";
-  } else {
-    x.className = "nav";
-  }
+    var x = document.getElementById("nav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
 }
 let days = document.getElementsByClassName("day");
 let daysArray = Array.from(days);
 
-function correct(){
-    
-}
+daysArray.forEach(function(current) {
+    current.addEventListener("click", function() {
+        if (current.classList.contains("purpleHover")){
+            current.classList.remove("purpleHover")
+            current.classList.add("correct")
+            current.addEventListener("click",)
+        }
+        else{
+            current.classList.remove("purpleHover")
+            current.classList.add("correct")
+        }
+    });
+})
