37669ab48e4a524e04949ed06623e4c4789ac8b6
Dave Marong <kmarong@hotmail.com>
2021-01-29 00:01:45 +0100

    Created a cookie
    - Now a cookie with a number id being printed everytime a day is clikced. next step is to make

---
+++ myChallenge.js
@@ -0,0 +1,83 @@
+/*-------Cookie for marked days----------*/
+function createCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name,"",-1);
+}
+
+/*-------Style "completed" and "uncompleted" mark on days----------*/
+let days = document.getElementsByClassName("day0");
+let daysArray = Array.from(days);
+
+daysArray.forEach(function(current) {
+    current.classList.remove("day0");
+    current.addEventListener("click", function() {
+        if (current.classList.contains("purpleHover")){
+            current.classList.remove("purpleHover")
+            current.classList.remove("reverseAnimation")
+            current.classList.add("correct")
+            let x = current.id;
+            createCookie('day'+x,x,0);
+        }
+        else{
+            current.classList.add("purpleHover")
+            current.classList.add("reverseAnimation")
+            current.classList.remove("correct")
+        }
+    });
+})
+
+/*-------Create new calendar----------*/
+let calendar = document.querySelector("#calendar");
+let clone = calendar.cloneNode(true);
+let createNewBtn = document.querySelector("#createCalendar");
+
+function newId(){ 
+
+    let newNumber = 2;
+    clone.id = "calendar" + newNumber;
+    newNumber += 1;
+    calendar.after(clone);
+
+    let days2 = document.getElementsByClassName("day0");
+    let daysArray2 = Array.from(days2);
+    createNewBtn.addEventListener("click", newId);
+    daysArray2.forEach(function(current) {
+        current.classList.remove("day0");
+        current.addEventListener("click", function() {
+            if (current.classList.contains("purpleHover")){
+                current.classList.remove("purpleHover")
+                current.classList.add("correct")
+            }
+            else{
+                current.classList.add("purpleHover")
+                current.classList.remove("correct")
+            }
+        });
+    })
+
+}
+createNewBtn.addEventListener("click", newId);
+
+
+
+
--- script.js
+++ script.js
@@ -9,60 +9,51 @@ function myFunction() {
 }
 
 /*----------------------MY CHALLENGE----------*/
-let calendar = document.querySelector("#calendar");
-let createNewBtn = document.querySelector("#createCalendar");
-let clone = calendar.cloneNode(true);
 
-/*-------Style "completed" and "uncompleted" mark on days----------*/
-let days = document.getElementsByClassName("day0");
-let daysArray = Array.from(days);
+/*-------Create challenge name----------*/
 
-daysArray.forEach(function(current) {
-    current.classList.remove("day0");
-    current.addEventListener("click", function() {
-        if (current.classList.contains("purpleHover")){
-            current.classList.remove("purpleHover")
-            current.classList.remove("reverseAnimation")
-            current.classList.add("correct")
-        }
-        else{
-            current.classList.add("purpleHover")
-            current.classList.add("reverseAnimation")
-            current.classList.remove("correct")
-        }
-    });
-})
+let btn_noFap = document.querySelector(".btn-noFap");
+let btn_yoga = document.querySelector(".btn-yoga");
+let btn_noSugar = document.querySelector(".btn-noSugar");
+let challengeName = document.querySelector(".challengeName");
+
+btn_noFap.addEventListener("click", function(){
+    document.cookie = "ChallengeName=No Fap";
+       })
 
 
-/*-------Create new calendar----------*/
 
 
-function newId(){ 
 
-    let newNumber = 2;
-    clone.id = "calendar" + newNumber;
-    newNumber += 1;
-    calendar.after(clone);
 
-    let days2 = document.getElementsByClassName("day0");
-    let daysArray2 = Array.from(days2);
-    createNewBtn.addEventListener("click", newId);
-    daysArray2.forEach(function(current) {
-        current.classList.remove("day0");
-        current.addEventListener("click", function() {
-            if (current.classList.contains("purpleHover")){
-                current.classList.remove("purpleHover")
-                current.classList.add("correct")
-            }
-            else{
-                current.classList.add("purpleHover")
-                current.classList.remove("correct")
-            }
-        });
-    })
 
-}
-createNewBtn.addEventListener("click", newId);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
