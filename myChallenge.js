294f0a56d66097cd6d5f3d03ebd53ab93f186860
Dave Marong <kmarong@hotmail.com>
2021-02-05 20:17:34 +0100

    Fixed cookie problem

--- myChallenge.js
+++ myChallenge.js
@@ -2,50 +2,51 @@
 let days = document.getElementsByClassName("day0");
 let daysArray = Array.from(days);
 
-daysArray.forEach(function(current) {
-    current.classList.remove("day0");
-    current.addEventListener("click", function() {
-        let x = current.id;
-        if (current.classList.contains("purpleHover")){
-            current.classList.remove("purpleHover")
-            current.classList.remove("reverseAnimation")
-            current.classList.add("correct")
-            createCookie('day'+x,x,100);
-        }
-        else{
-            current.classList.add("purpleHover")
-            current.classList.add("reverseAnimation")
-            current.classList.remove("correct")
-            eraseCookie('day'+x)
-        }
-    });
-})
+daysArray.forEach(function (current) {
+  current.classList.remove("day0");
+  current.addEventListener("click", function () {
+    let x = current.id;
+    if (current.classList.contains("purpleHover")) {
+      current.classList.remove("purpleHover");
+      current.classList.remove("reverseAnimation");
+      current.classList.add("correct");
+      createCookie("day" + x, x, 100);
+    } else {
+      current.classList.add("purpleHover");
+      current.classList.add("reverseAnimation");
+      current.classList.remove("correct");
+      eraseCookie("day" + x);
+    }
+  });
+});
 
 /*-------Creates and edits challenge name(h1) and description cookie----------*/
 let challengeName = document.querySelector(".challengeName__h1");
-let challengeDescription = document.querySelector(".challengeDescription__textarea");
+let challengeDescription = document.querySelector(
+  ".challengeDescription__textarea"
+);
 let editBtn = document.querySelector(".editButton");
 let saveBtn = document.querySelector(".saveButton");
 
-editBtn.addEventListener("click", function(){
-    challengeName.setAttribute("contenteditable", true);
-    challengeName.classList.add("editable");
-    challengeName.focus();
-    challengeDescription.setAttribute("contenteditable", true);
-    challengeDescription.classList.add("editable");
-    saveBtn.style.visibility = "visible";
-})
-saveBtn.addEventListener("click", function(){
-    let name = challengeName.textContent;
-    let description = challengeDescription.textContent;
-    createCookie("challengeName",name,100);
-    createCookie("challengeDescription",description,100);
-    challengeName.setAttribute("contenteditable", false);
-    challengeDescription.setAttribute("contenteditable", false);
-    challengeName.classList.remove("editable");
-    challengeDescription.classList.remove("editable");
-    saveBtn.style.visibility = "hidden";
-})
+editBtn.addEventListener("click", function () {
+  challengeName.setAttribute("contenteditable", true);
+  challengeName.classList.add("editable");
+  challengeName.focus();
+  challengeDescription.setAttribute("contenteditable", true);
+  challengeDescription.classList.add("editable");
+  saveBtn.style.visibility = "visible";
+});
+saveBtn.addEventListener("click", function () {
+  let name = challengeName.textContent;
+  let description = challengeDescription.textContent;
+  createCookie("challengeName", name, 100);
+  createCookie("challengeDescription", description, 100);
+  challengeName.setAttribute("contenteditable", false);
+  challengeDescription.setAttribute("contenteditable", false);
+  challengeName.classList.remove("editable");
+  challengeDescription.classList.remove("editable");
+  saveBtn.style.visibility = "hidden";
+});
 /*challengeName.addEventListener("input", function(){
     let name = challengeName.textContent;
     createCookie("name",name,100);
@@ -61,21 +62,26 @@ challengeDescription.addEventListener("input", function(){
 
 /*-------Check for cookies when page load----------*/
 let numberX = 1;
-daysArray.forEach(function(current){
-    let name = readCookie("day"+numberX);
-    if (name == current.id){
-        current.classList.remove("purpleHover")
-        current.classList.remove("reverseAnimation")
-        current.classList.add("correct")
-    };
-    numberX += 1;
-})
+daysArray.forEach(function (current) {
+  let name = readCookie("day" + numberX);
+  if (name == current.id) {
+    current.classList.remove("purpleHover");
+    current.classList.remove("reverseAnimation");
+    current.classList.add("correct");
+  }
+  numberX += 1;
+});
 
-function updateChallengeInfo(){
-    let name = readCookie('challengeName');
-    let description = readCookie('challengeDescription');
+function updateChallengeInfo() {
+  let name = readCookie("challengeName");
+  let description = readCookie("challengeDescription");
+  // if (name !== null) {
+  if (name !== null) {
     challengeName.textContent = name;
+  }
+  if (description !== null) {
     challengeDescription.textContent = description;
+  }
 }
 /*function updateChallengeName(){
     let name = readCookie("name");
@@ -84,16 +90,15 @@ function updateChallengeInfo(){
 */
 let calendar = document.querySelector("#calendar");
 
-function removeChallengeTypeClasses(){
-    calendar.classList.remove(".calendar-yoga")
-    calendar.classList.remove(".calendar-nofap")
-    calendar.classList.remove(".calendar-junkfood")
+function removeChallengeTypeClasses() {
+  calendar.classList.remove(".calendar-yoga");
+  calendar.classList.remove(".calendar-nofap");
+  calendar.classList.remove(".calendar-junkfood");
 }
-
-function updateChallengeType(){
-    removeChallengeTypeClasses();
-    let challengeType = readCookie('challengeType');
-    calendar.classList.add(challengeType);
+function updateChallengeType() {
+  removeChallengeTypeClasses();
+  let challengeType = readCookie("challengeType");
+  calendar.classList.add(challengeType);
 }
 
 /*function updateDescription(){
@@ -127,7 +132,6 @@ let rotateBackward = document.querySelector(".rotateBackward");
 rotateForward.addEventListener("click", rotateWeekDaysForward);
 rotateBackward.addEventListener("click", rotateWeekDaysBackwards);
 */
-
 
 /*-------Create new calendar----------*/
 let clone = calendar.cloneNode(true);
@@ -163,5 +167,3 @@ function newId(){
 createNewBtn.addEventListener("click", newId);
 
 */
-
-
