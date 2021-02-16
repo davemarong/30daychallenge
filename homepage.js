e689d9a7c6c655916af18e2dfff69dfdb71b6a74
Dave Marong <kmarong@hotmail.com>
2021-02-16 19:12:16 +0100

    Done with the mechanics of navbar
    - Only thing missing is the colors and style. 
    - Will try to get an animation on this as well

--- homepage.js
+++ homepage.js
@@ -24,3 +24,11 @@ for (let i = 0; i < accordion__item.length; i++) {
     }
   });
 }
+function toggleNavbar() {
+  var x = document.querySelector(".nav-mobile-ul");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
